49992aea6dce97c7c85c688c7028dc0ea04ce519
Naman Gogia <namangogia2001@gmail.com>
2022-08-05 20:31:34 +0530

    node fs module

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function hello() {
-  console.log("Hello Node.Js! Trying it for the first time");
-}
+const fs = require("fs");
 
-hello();
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  function (err) {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+
+fs.readFile("sample.txt", function (err, data) {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+fs.appendFile("sample.txt", " This is my updated content", function (err) {
+  if (err) throw err;
+  console.log("File updated!");
+});
+
+fs.rename("sample.txt", "test.txt", function (err) {
+  if (err) throw err;
+  console.log("File name updated!");
+});
+
+// Deleting files
+fs.unlink("test.txt", function (err) {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!");
+});
